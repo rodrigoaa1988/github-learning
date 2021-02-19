d1ec75b973f8e4bdfb2e37fb11a9ff7fd6fa268f
rodrigoaa1988 <rodrigoanjosalmeida@gmail.com>
2021-02-19 15:32:55 -0300

    mensagem de boa noite

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,10 @@
 package application;
 
 public class Program {
-
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		System.out.println("teste");
 		System.out.println("Bom dia! Novo teste!");
 		System.out.println("Boa tarde!");
-		
-
+		System.out.println("Boa noite!");
 	}
-
 }
